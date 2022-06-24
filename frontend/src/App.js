0c51d51e7d0de77e3c2fc8746ac8afc54889f8db
Chrispy <christiancarteno@hotmail.com>
2022-06-24 14:13:01 -0700

    Init

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <h1>Hello from App</h1>
+  );
+}
+
+export default App;
